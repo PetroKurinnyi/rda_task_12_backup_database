1513be7d024562de2fb32c0c1335fd945d358505
Petro Kurinnyi <petro.kurinnyi@gmail.com>
2026-01-11 14:08:03 +0200

    solution

--- task.sh
+++ task.sh
@@ -1,1 +1,9 @@
 #! /bin/bash
+
+username="${DB_USER}"
+password="${DB_PASSWORD}"
+
+mysqldump -u"$username" -p"$password" ShopDB > shopdb_full.sql
+mysql -u"$username" -p"$password" ShopDBReserve < shopdb_full.sql
+mysqldump -u"$username" -p"$password" --no-create-info ShopDB Products > shopdb_data.sql
+mysql -u"$username" -p"$password" ShopDBDevelopment < shopdb_data.sql
