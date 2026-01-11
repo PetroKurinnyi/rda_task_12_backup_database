cc64ee7b671a67e00d8c48364a09cf98b4b29969
Petro Kurinnyi <petro.kurinnyi@gmail.com>
2026-01-11 14:11:50 +0200

    PR review

--- task.sh
+++ task.sh
@@ -5,5 +5,5 @@ password="${DB_PASSWORD}"
 
 mysqldump -u"$username" -p"$password" ShopDB > shopdb_full.sql
 mysql -u"$username" -p"$password" ShopDBReserve < shopdb_full.sql
-mysqldump -u"$username" -p"$password" --no-create-info ShopDB Products > shopdb_data.sql
+mysqldump -u"$username" -p"$password" --no-create-info ShopDB > shopdb_data.sql
 mysql -u"$username" -p"$password" ShopDBDevelopment < shopdb_data.sql
